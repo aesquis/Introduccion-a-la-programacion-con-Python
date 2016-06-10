2aa07590bee10ec38e355631a5894fdce1570d5b
aesquis <aesquis@gmail.com>
2016-06-10 10:43:22 +0200

    Create 02-Visualización-de-texto\Module2DisplayText.py

---
+++ 02-Visualización-de-texto\Module2DisplayText.py
@@ -0,0 +1,32 @@
+print('The capybara is the worlds largest rodent')
+print('The capybara likes to lives in groups')
+print("The capybara can swim")
+print("It's a beautiful  day")
+print('Entre "cometes"')   
+
+# \n fa un intro 
+print('Hello \nque ')
+# \t un espai
+print('Hello \t World')
+print("""Hickory Dickory Dock!
+The mouse ran up the clock""")
+print('''Hickory Dickory Dock!
+The mouse ran up the clock''')
+print('here is a double quote "' + "here is a single quote '")
+print("or you can just do this \" does that work")
+print("A Slash \ it works")
+print('But if I wnat \\news')
+
+print('There once was a movie star icon')
+print('who preferred to sleep with the light on.')
+print('They learned how to code')
+print('a device that sure glowed')
+print('and lit up the night using Python!') 
+
+print("""There once was a movie star icon
+who preferred to sleep with the light on.
+They learned how to code
+a device that sure glowed
+and lit up the night using Python!""")
+
+print('There once was a movie star icon \nwho preferred to sleep with the light on. \nThey learned how to code \na device that sure glowed \nand lit up the night using Python!')
