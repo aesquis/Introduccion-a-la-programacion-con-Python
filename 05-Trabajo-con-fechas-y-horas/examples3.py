84d49d1a533e4cb405f5dca29abb70ff8b1264ad
aesquis <aesquis@gmail.com>
2016-06-13 17:59:32 +0200

    Create examples3.py

---
+++ 05-Trabajo-con-fechas-y-horas/examples3.py
@@ -0,0 +1,17 @@
+import datetime    
+#Get time and print time
+
+
+currentTime = datetime.datetime.now()
+print(currentTime)
+print(currentTime.hour)
+print(currentTime.minute)
+print(currentTime.second)
+
+
+print (datetime.datetime.strftime(currentTime, '%H:%M'))
+# %H Hours (24 hr clock)
+# %l Hours (12 hr clock)
+# %p AM or PM
+# %m Minutes
+# %S Seconds
