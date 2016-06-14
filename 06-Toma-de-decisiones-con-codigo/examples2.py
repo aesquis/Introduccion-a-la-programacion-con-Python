29589c9cb76077553b5cfab04ee18ed509aa1697
aesquis <aesquis@gmail.com>
2016-06-14 17:16:37 +0200

    Create examples2.py

---
+++ 06-Toma-de-decisiones-con-codigo/examples2.py
@@ -0,0 +1,13 @@
+#coding:windows-1252
+#deposit = 150
+#if deposit > 100 :
+#    print("You get a free toaster!")
+#print("Have a nice day")
+
+#if with nmbers
+#he de convertir la variable en enter per a poder comparar, en cas contrari donarà error per
+# estar comparant una cadena contra un número
+deposit = int(input("How much do you want to deposit? "))
+if deposit > 100 :
+    print("Enjoy your toaster!")
+print("Have a nice day")
