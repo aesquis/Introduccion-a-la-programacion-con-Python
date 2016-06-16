a7834173989e86eb7a71917565f21c0586d139be
aesquis <aesquis@gmail.com>
2016-06-16 11:16:44 +0200

    Create examples.py

---
+++ 07-Decisiones-complejas-con-codigo/examples.py
@@ -0,0 +1,10 @@
+# The "elif" alloes you to check fo different values
+
+country = input("Where ara you from? ").upper()
+
+if country == "CANADA" :
+    print("Hello")
+elif country == "GERMANY" : 
+    print("Guten Tag")
+elif country == "FRANCE" :
+    print("Bonjour")
