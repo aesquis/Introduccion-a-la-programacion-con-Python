208081d7060b5da8eea9762874c94f85fa7ed34a
aesquis <aesquis@gmail.com>
2016-06-22 17:22:44 +0200

    Create examples2.py

---
+++ 07-Decisiones-complejas-con-codigo/examples2.py
@@ -0,0 +1,11 @@
+team = input("Enter your fabourite football team: ").upper()
+
+if team == "MADRID" :
+    print("Puta merda equip")
+elif team == "BARCELONA" :
+    print("Millor equip de la historia")
+elif team == "ATLETICO" :
+    print("Aquest any ho pagareu")
+else :
+    print("No tens equip")
+
