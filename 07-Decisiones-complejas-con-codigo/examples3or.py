7af3a58ce4d851678c82e96c2947a830058f8cb6
aesquis <aesquis@gmail.com>
2016-06-23 10:02:45 +0200

    Create examples3or.py

---
+++ 07-Decisiones-complejas-con-codigo/examples3or.py
@@ -0,0 +1,16 @@
+saturday = True
+sunday = False
+
+if saturday or sunday:
+    print("Ok, you can sleep in")
+
+team = input("Enter your favourite hockey team; ").upper()
+sport = input("Enter your favourite sport: ").upper()
+
+if sport == "FOOTBALL" and team == "BARCELONA":
+    print("putu amo")
+elif team == "MADRID" or team =="ATLETICO":
+    print("FCK")
+
+else:
+    print("putu looser")
