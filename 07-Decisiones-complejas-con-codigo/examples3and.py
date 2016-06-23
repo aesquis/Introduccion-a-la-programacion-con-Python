dca437cd0d652f02839639acfa9cc3dda62f019b
aesquis <aesquis@gmail.com>
2016-06-23 08:29:33 +0200

    Create examples3and.py

---
+++ 07-Decisiones-complejas-con-codigo/examples3and.py
@@ -0,0 +1,13 @@
+#wonLottery = True
+#bigWin = True
+
+#if wonLottery and bigWin :
+#    print("you can retire! ")
+
+team = input("Enter your favourite hockey team; ").upper()
+sport = input("Enter your favourite sport: ").upper()
+
+if sport == "FOOTBALL" and team == "BARCELONA":
+    print("putu amo")
+else:
+    print("putu looser")
