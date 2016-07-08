18d80e21fce2ef36585f260c4dbbdc624c875649
aesquis <aesquis@gmail.com>
2016-07-08 10:48:12 +0200

    Create exercici.py

---
+++ 10-Recordatorios-de-listas/exercici.py
@@ -0,0 +1,24 @@
+#Llista de convidats a la festa
+
+#definim llista convidats
+convidats = ['']
+print ('Entra el nom dels convidats, quan tinguis la llista complerta escui FI')
+#definim variable intpConv per que no dongi error
+intpConv = ('')
+
+#Fem loop per a recollir dades
+
+#Mentre la variable intpConv sigui diferent de Fi s'executa el loop
+while intpConv != 'FI':
+    intpConv = input('Convidat: ')
+    #afegim el valor de la variable intpConv a la llista convidats
+    convidats.append(intpConv)
+#Eliminar registre FI de la llista FI
+convidats.remove('FI')
+#Ordenem la llista
+convidats.sort()
+print('Doncs aquests son tots els convidats de la teva llista')
+#Fem un loop per a mostrar la llista
+for llista in convidats:
+    print(llista)
+
