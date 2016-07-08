49b9c85c2f9f5d4b3036b5762d6160d188242d07
aesquis <aesquis@gmail.com>
2016-07-08 14:13:24 +0200

    Update exercici.py

--- 11-Como-guardar-informacion-en-archivos/exercici.py
+++ 11-Como-guardar-informacion-en-archivos/exercici.py
@@ -13,6 +13,9 @@ nombre = 1
 file = open(fileName, mode = WRITE)
 print('Entra el nom i edat de cada un dels convidats\n')
 #Mentre la variable nomConv sigui diferent de FI s'executa el loop
+#Un dels problemes principals era que al escriure FI, encar em demanava l'edat un cop i això ho escribia al fitxer
+#ara primer inicio el loop i amb l'if posterior controlo que en cas de que el nom sigui FI, ho puc tallar al moment
+#sense escriure res al fitxer ni demanar l'edat del FI
 while nomConv != 'FI':
     nomConv = input('Nom %d: ' %nombre)
     if nomConv != 'FI':
