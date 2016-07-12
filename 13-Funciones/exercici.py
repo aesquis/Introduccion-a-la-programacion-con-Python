3700e002c897a1117ccc585f848e94eb8199abd4
aesquis <aesquis@gmail.com>
2016-07-12 13:34:44 +0200

    Create exercici.py

---
+++ 13-Funciones/exercici.py
@@ -0,0 +1,24 @@
+def crearFitxer():
+    nom = nomArxiu()
+    data = dataArxiu()
+    creaArxiu(nom, data)
+    return
+
+def nomArxiu():
+    nom = input('Com vols que es digui el fitxer? ')
+    return nom
+
+def dataArxiu():
+    data = input('Que vols escriure en el fitxer? ')
+    return data
+
+
+def creaArxiu(nom, data):
+    fileName = nom
+    WRITE = 'w'
+    file = open(fileName, mode = WRITE)
+    file.write(data)
+    file.close()
+
+crearFitxer()
+print('Fitxer ceart correctament')
