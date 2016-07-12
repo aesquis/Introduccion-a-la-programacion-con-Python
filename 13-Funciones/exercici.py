c361538436f4f7694400f956f0e43e75cabde9d8
aesquis <aesquis@gmail.com>
2016-07-12 13:44:43 +0200

    Update exercici.py

--- 13-Funciones/exercici.py
+++ 13-Funciones/exercici.py
@@ -22,3 +22,11 @@ def creaArxiu(nom, data):
 
 crearFitxer()
 print('Fitxer ceart correctament')
+
+#Microsoft----------------------
+def writeText(data, filename):
+    file = open(filename, mode = 'w')
+    file.write(data)
+    return
+
+writeText('Hello, world.', 'c:\\users\\chharri\\documents\\hello.txt')
